3fb82d0f3d0e6ab9c6e9f0a2849e65a3af0849e5
Daniel Babbev <daniel@fair.poker>
2020-10-10 18:04:22 +0200

    better messages

--- index.js
+++ index.js
@@ -11,45 +11,48 @@ const apiEndpoint = process.env.ENDPOINT !== undefined ? process.env.ENDPOINT : 
 const groupID = process.env.GROUP_ID;
 const refreshIntervalSec = process.env.REFRESH_INTERVAL_SEC !== undefined ? process.env.REFRESH_INTERVAL_SEC : 10;
 
-let prevRunningTablesCount = 0;
-let prevWaitingForPlayersCount = 0;
+let prevTablesRunning = 0;
+let perTablesWaitingForPlayers = 0;
 let lastMessageSent = '';
 let lastMessageSentTime = new Date();
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(process.env.TOKEN, {polling: true});
 
-const pluralizeWordAndArticle = (count, word) => count > 1 ? `${count} ${word}s are` : `${count} ${word} is`;
-const getMotivationPhrase = () => `${motivationPhrases[Math.floor(Math.random() * motivationPhrases.length)]} ${emojiLibrary[Math.floor(Math.random() * emojiLibrary.length)]}`;
+const getRandMotivationPhrase = () => `${motivationPhrases[Math.floor(Math.random() * motivationPhrases.length)]} ${emojiLibrary[Math.floor(Math.random() * emojiLibrary.length)]}`;
+const getBits = (sat) => sat / 100;
 
 const alertGroup = (tables) => {
-  const waitingForPlayers = tables.filter(x => x.playersSeatedCount === 1);
-  const runningTables = tables.filter(x => x.playersSeatedCount > 1);
-
-  const waitingForPlayersCount = waitingForPlayers.length;
-  const runningTablesCount = runningTables.length;
+  const tablesWaitingForPlayers = tables.filter(x => x.playersSeatedCount === 1).length;
+  const tablesRunning = tables.filter(x => x.playersSeatedCount > 1).length;
 
 
+  // Printout of the lobby
   let message = '';
-  if (waitingForPlayersCount > prevWaitingForPlayersCount && runningTablesCount > 0 || runningTablesCount > prevRunningTablesCount && waitingForPlayersCount > 0) {
-    message = `${pluralizeWordAndArticle(waitingForPlayersCount, 'table')} waiting for players to join and ${pluralizeWordAndArticle(runningTablesCount, 'table')} running.`;
-  } else if (waitingForPlayersCount > prevWaitingForPlayersCount) {
-      message = `${pluralizeWordAndArticle(waitingForPlayersCount, 'table')} waiting for players.`;
-  } else if (runningTablesCount > prevRunningTablesCount) {
-      message = `${pluralizeWordAndArticle(runningTablesCount, 'table')} running.`;
-  }
+  tables
+    .filter(x => x.playersSeatedCount > 0)
+    .sort(x => x.playersSeatedCount)
+    .forEach(x => {
+      if (x.playersSeatedCount > 1) {
+        message += `${x.playersSeatedCount} players at ${getBits(x.smallBlind)}/${getBits(x.bigBlind)} (${x.gameMode})\n`
+      } else {
+        message += `1 player is waiting at ${getBits(x.smallBlind)}/${getBits(x.bigBlind)} (${x.gameMode})\n`
+      }
+    });
 
   // Send a mesage if the message is different from the last sent one 
   // or if the message is the same wait at least 1h before repeating it
   const minFromLastMessageSent = (new Date() - lastMessageSentTime) / (1000 * 60);
-  if (message !== '' && (message !== lastMessageSent || minFromLastMessageSent >= 60)) {
+  if (message !== ''
+    && (tablesRunning > prevTablesRunning || tablesWaitingForPlayers > perTablesWaitingForPlayers || minFromLastMessageSent >= 60) // If the number of tables has increased
+    && (message !== lastMessageSent || minFromLastMessageSent >= 60)) {
     lastMessageSent = message;
     lastMessageSentTime = new Date();
-    bot.sendMessage(groupID, `${message} ${getMotivationPhrase()}`);
+    bot.sendMessage(groupID, `${message}${getRandMotivationPhrase()}`);
   }
 
-  prevWaitingForPlayersCount = waitingForPlayersCount;
-  prevRunningTablesCount = runningTablesCount;
+  perTablesWaitingForPlayers = tablesWaitingForPlayers;
+  prevTablesRunning = tablesRunning;
 }
 
 const checkRunningTables = () => {
@@ -61,4 +64,5 @@ const checkRunningTables = () => {
     .catch(err => console.log('error getting table data', err));
 }
 
+checkRunningTables();
 setInterval(checkRunningTables, refreshIntervalSec * 1000);
