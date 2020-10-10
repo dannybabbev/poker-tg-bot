7d9af226d160edf8e1eddaa81f5021e731865b8d
Daniel Babbev <daniel@fair.poker>
2020-10-10 13:12:01 +0200

    fix logic, add cool phrases

---
+++ README.md
@@ -0,0 +1,5 @@
+## Fair Poker Telegram Bot
+
+This bot will notify a Telegram group when games are running on (Fair.Poker)[https://fair.poker/].
+
+Just set the proper environment variables for your Telegram group and bot. Refer to `env` for the variable names.
--- index.js
+++ index.js
@@ -4,43 +4,52 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const emojiLibrary = ['🤑', '🕶', '😎', '🤩', '♠️', '💰', '💲💲', '🔥', '🙈', '😈'];
+const motivationPhrases = ['Go play', 'Join in', 'Come get their money', 'Fishing time'];
+
 const apiEndpoint = process.env.ENDPOINT !== undefined ? process.env.ENDPOINT : 'https://play.fair.poker/lobby-data';
 const groupID = process.env.GROUP_ID;
-const refreshIntervalSec = 10;
+const refreshIntervalSec = process.env.REFRESH_INTERVAL_SEC !== undefined ? process.env.REFRESH_INTERVAL_SEC : 10;
 
-let runningTablesCount = 0;
-let waitingForPlayersCount = 0;
+let prevRunningTablesCount = 0;
+let prevWaitingForPlayersCount = 0;
 let lastMessageSent = '';
+let lastMessageSentTime = new Date();
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(process.env.TOKEN, {polling: true});
 
 const pluralizeWordAndArticle = (count, word) => count > 1 ? `${count} ${word}s are` : `${count} ${word} is`;
+const getMotivationPhrase = () => `${motivationPhrases[Math.floor(Math.random() * motivationPhrases.length)]} ${emojiLibrary[Math.floor(Math.random() * emojiLibrary.length)]}`;
 
 const alertGroup = (tables) => {
   const waitingForPlayers = tables.filter(x => x.playersSeatedCount === 1);
   const runningTables = tables.filter(x => x.playersSeatedCount > 1);
 
-  const newWaitingForPlayersCount = waitingForPlayers.length;
-  const newRunningTablesCount = runningTables.length;
+  const waitingForPlayersCount = waitingForPlayers.length;
+  const runningTablesCount = runningTables.length;
 
 
   let message = '';
-  if (newWaitingForPlayersCount > waitingForPlayersCount && newRunningTablesCount > runningTablesCount) {
-    message = `${pluralizeWordAndArticle(newWaitingForPlayersCount, 'table')} waiting for players to join and ${pluralizeWordAndArticle(newRunningTablesCount, 'table')} running`;
-  } else if (newWaitingForPlayersCount > waitingForPlayersCount) {
-    message = `${pluralizeWordAndArticle(newWaitingForPlayersCount, 'table')} waiting for players. Join in!`;
-  } else if (newRunningTablesCount > runningTablesCount) {
-    message = `${pluralizeWordAndArticle(newRunningTablesCount, 'table')} running. Join in!`;
+  if (waitingForPlayersCount > prevWaitingForPlayersCount && runningTablesCount > 0 || runningTablesCount > prevRunningTablesCount && waitingForPlayersCount > 0) {
+    message = `${pluralizeWordAndArticle(waitingForPlayersCount, 'table')} waiting for players to join and ${pluralizeWordAndArticle(runningTablesCount, 'table')} running.`;
+  } else if (waitingForPlayersCount > prevWaitingForPlayersCount) {
+      message = `${pluralizeWordAndArticle(waitingForPlayersCount, 'table')} waiting for players.`;
+  } else if (runningTablesCount > prevRunningTablesCount) {
+      message = `${pluralizeWordAndArticle(runningTablesCount, 'table')} running.`;
   }
 
-  if (message !== '' && message !== lastMessageSent) {
+  // Send a mesage if the message is different from the last sent one 
+  // or if the message is the same wait at least 1h before repeating it
+  const minFromLastMessageSent = (new Date() - lastMessageSentTime) / (1000 * 60);
+  if (message !== '' && (message !== lastMessageSent || minFromLastMessageSent >= 60)) {
     lastMessageSent = message;
-    bot.sendMessage(groupID, message);
+    lastMessageSentTime = new Date();
+    bot.sendMessage(groupID, `${message} ${getMotivationPhrase()}`);
   }
 
-  waitingForPlayersCount = newWaitingForPlayersCount;
-  runningTablesCount = newRunningTablesCount;
+  prevWaitingForPlayersCount = waitingForPlayersCount;
+  prevRunningTablesCount = runningTablesCount;
 }
 
 const checkRunningTables = () => {
